35528130df5d04ef85f1b642fb0ab5f0f5d81d7a
Mhirax <olowookeremiracle14@gmail.com>
2025-11-21 14:35:27 +0100

    introducing arrays

--- Arrays.js
+++ Arrays.js
@@ -1,9 +1,13 @@
-// Introducing ARRAYS 
-Arrays is our very first data structure. a data structure which is a collection of data 
-How do we make array in JavaScript? It is simple: we use square brackets 
+// Introducing ARRAYS
+//Arrays is our very first data structure. a data structure which is a collection of data How do we make array in JavaScript? It is simple: we use square brackets 
 
 
-let colors = [];
+let colors = [];             //this is an empty Arrayscd
+
+
+
+
+
 
 // Arrays Random access
 
